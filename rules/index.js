b3d8111694b5e1eac1b135d61355325cb26befd7
antiny <an@tinypulse.com>
2017-12-02 13:45:31 +0700

    add rule check meta tag

--- rules/index.js
+++ rules/index.js
@@ -2,5 +2,7 @@
 
 module.exports = [
     require('./images-without-alt.js'),
-    require('./links_without_rel.js')
+    require('./links_without_rel.js'),
+    require('./head-title-tag.js'),
+    require('./head-meta-description')
 ];
