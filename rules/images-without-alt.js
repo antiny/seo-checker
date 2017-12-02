1dd286a25fc5d72bf5b1142c77629e488d68831a
antiny <an@tinypulse.com>
2017-12-02 15:52:42 +0700

    fix typo

--- rules/images-without-alt.js
+++ rules/images-without-alt.js
@@ -5,7 +5,7 @@ const util = require('./util');
 module.exports = function(dom) {
     var linksWithoutRel = util.countTags(dom, 'a:not([rel])');
     if (linksWithoutRel > 0) {
-        return util.error(`${linksWithoutRel.length} links do not have rel attribute`);
+        return util.error(`${linksWithoutRel} links do not have rel attribute`);
     }
     return util.success;
 };
--- rules/links_without_rel.js
+++ rules/links_without_rel.js
@@ -5,7 +5,7 @@ const util = require('./util');
 module.exports = function(dom) {
     var imageWithoutAlt = util.countTags(dom, 'img:not([alt])');
     if (imageWithoutAlt > 0) {
-        return util.error(`${imageWithoutAlt.length} images do not have alt attribute`);
+        return util.error(`${imageWithoutAlt} images do not have alt attribute`);
     }
     return util.success;
 };
